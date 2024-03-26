a4fdf1b0caa7ed831b2a1e66873b47a2d7262b5a
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2024-03-26 15:18:59 -0400

    Code refactor: remove return key word

--- filters.rb
+++ filters.rb
@@ -1,8 +1,6 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
-require 'active_support/all'
-
 # Find candidate by ID
 def find(id)
   @candidates.each do |candidate|
@@ -10,12 +8,12 @@ def find(id)
       return candidate
     end
   end
-  return nil
+  nil
 end
 
 # Returns true if the candidate has 2 years of experience or more, returns false otherwise
 def experienced?(candidate)
-  return candidate[:years_of_experience] >= 2
+  candidate[:years_of_experience] >= 2
 end
 
 # Returns a subset of the candidates that are experienced, have 100 or more Github points, know at least Ruby or Python, applied in the last 15 days, are over the age of 17 (18+)
@@ -26,32 +24,31 @@ def qualified_candidates(candidates)
       qualified_candidates.push(candidate)
     end
   end
-  return qualified_candidates
+  qualified_candidates
 end
 
 # Returns true if the candidate has 100 github points or more, returns false otherwise
 def enough_points?(candidate)
-  return candidate[:github_points] >= 100
+  candidate[:github_points] >= 100
 end
 
 # Returns true if the candidate knows at least Ruby or Python, returns false otherwise
 def ruby_or_python?(candidate)
-  return candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')
+  candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')
 end
 
 # Returns true if the candidate applied in the last 15 days, returns false otherwise
 def recently_applied?(candidate)
-  days_ago = (Date.today - candidate[:date_applied]).to_i
-  return days_ago <= 15
+  (Date.today - candidate[:date_applied]).to_i <= 15
 end
 
 # Returns true if the candidate is over the age of 17 (18+), returns false otherwise
 def age_18_plus?(candidate)
-  return candidate[:age] > 17
+  candidate[:age] > 17
 end
 
 # Sort candidates by years of experience in descending order
 # If years of experience are the same, sort by github points in descending order
 def ordered_by_qualifications(candidates)
- return candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+ candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
 end

abb106995dde60b41dd8c77d70ba9e32abd31844
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2024-03-26 10:56:59 -0400

    Implement methods: experienced, find by id

--- filters.rb
+++ filters.rb
@@ -1,16 +1,25 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
+require 'active_support/all'
+
+# Find candidate by ID
 def find(id)
-    # Your code Here
+  @candidates.each do |candidate|
+    if candidate[:id] == id
+      return candidate
+    end
   end
+  return nil
+end
+
+# Returns true if the candidate has 2 years of experience or more, returns false otherwise
+def experienced?(candidate)
+  return candidate[:years_of_experience] >= 2
+end
+
+def qualified_candidates(candidates)
+  # Your code Here
+end
   
-  def experienced?(candidate)
-    # Your code Here
-  end
-  
-  def qualified_candidates(candidates)
-    # Your code Here
-  end
-  
-  # More methods will go below
+# More methods will go below
